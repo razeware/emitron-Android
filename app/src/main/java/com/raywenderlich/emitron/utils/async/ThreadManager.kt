e611574bab3cbf789685d781d203b88ca36d191e
Rachit Mishra <twntee@gmail.com>
2019-07-22 02:28:31 +0530

    Move ThreadManager to /utils/async

--- app/src/main/java/com/raywenderlich/emitron/utils/async/ThreadManager.kt
+++ app/src/main/java/com/raywenderlich/emitron/utils/async/ThreadManager.kt
@@ -1,4 +1,4 @@
-package com.raywenderlich.emitron.impl
+package com.raywenderlich.emitron.utils.async
 
 import kotlinx.coroutines.CoroutineDispatcher
 import java.util.concurrent.Executor
--- app/src/main/java/com/raywenderlich/emitron/utils/async/ThreadManagerImpl.kt
+++ app/src/main/java/com/raywenderlich/emitron/utils/async/ThreadManagerImpl.kt
@@ -1,11 +1,12 @@
-package com.raywenderlich.emitron.impl
+package com.raywenderlich.emitron.utils.async
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import java.util.concurrent.Executors
 import javax.inject.Inject
 
-class ThreadManagerImpl @Inject constructor() : ThreadManager {
+class ThreadManagerImpl @Inject constructor() :
+  ThreadManager {
 
   override val io: CoroutineDispatcher = Dispatchers.IO
 
