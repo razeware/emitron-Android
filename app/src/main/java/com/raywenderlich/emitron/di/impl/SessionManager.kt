893a81819e5581b727bb5be9c194640b4d7fc360
Rachit Mishra <twntee@gmail.com>
2019-07-22 02:10:11 +0530

    Remove redundant files

--- app/src/main/java/com/raywenderlich/emitron/di/impl/SessionManager.kt
+++
@@ -1,7 +0,0 @@
-package com.raywenderlich.emitron.di.impl
-
-interface SessionManager {
-  fun getUserEmailAddress(): String
-  fun getUserAccountId(): String
-  fun getUserName(): String // As in name ex. Nathan
-}
