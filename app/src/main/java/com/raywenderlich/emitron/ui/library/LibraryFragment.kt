42366a71739ce2e776685062dd5ee88a59600b55
Rachit Mishra <twntee@gmail.com>
2019-07-31 02:23:09 +0530

    Update library ui with view databinding

--- app/src/main/java/com/raywenderlich/emitron/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/raywenderlich/emitron/ui/library/LibraryFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.raywenderlich.emitron.MainViewModel
 import com.raywenderlich.emitron.R
+import com.raywenderlich.emitron.databinding.FragmentLibraryBinding
 import com.raywenderlich.emitron.di.modules.viewmodel.ViewModelFactory
+import com.raywenderlich.emitron.utils.extensions.setDataBindingView
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -22,11 +25,27 @@ class LibraryFragment : DaggerFragment() {
 
   private val parentViewModel: MainViewModel by activityViewModels { viewModelFactory }
 
+  private lateinit var binding: FragmentLibraryBinding
+
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.layout_styles, container, false)
+    binding = setDataBindingView(
+      inflater, R.layout.fragment_library, container
+    )
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    checkLogin()
+  }
+
+  private fun checkLogin() {
+    if (!parentViewModel.isAllowed()) {
+      findNavController().navigate(R.id.action_navigation_library_to_navigation_login)
+    }
   }
 }
