454e34842c0d4e48d65836c00f878e4383e6d433
Rachit Mishra <twntee@gmail.com>
2019-07-31 02:22:04 +0530

    Add settings flow

--- app/src/main/java/com/raywenderlich/emitron/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/raywenderlich/emitron/ui/settings/SettingsViewModel.kt
@@ -1,6 +1,19 @@
 package com.raywenderlich.emitron.ui.settings
 
 import androidx.lifecycle.ViewModel
+import com.raywenderlich.emitron.data.login.LoginRepository
 import javax.inject.Inject
 
-class SettingsViewModel @Inject constructor() : ViewModel()
+/**
+ * ViewModel for settings view
+ */
+class SettingsViewModel @Inject constructor(private val loginRepository: LoginRepository) :
+  ViewModel() {
+
+  /**
+   * Logout user
+   */
+  fun logout() {
+    loginRepository.deleteUser()
+  }
+}
