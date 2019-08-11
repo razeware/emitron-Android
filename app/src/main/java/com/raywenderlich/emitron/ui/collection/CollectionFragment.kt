406bb974105273e7ee1ce742536f8250e0e88f13
Rachit Mishra <twntee@gmail.com>
2019-08-11 14:43:52 +0530

    Update Collection/Filter UI to use databinding

--- app/src/main/java/com/raywenderlich/emitron/ui/collection/CollectionFragment.kt
+++ app/src/main/java/com/raywenderlich/emitron/ui/collection/CollectionFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.raywenderlich.emitron.R
+import com.raywenderlich.emitron.databinding.FragmentCollectionBinding
 import com.raywenderlich.emitron.di.modules.viewmodel.ViewModelFactory
+import com.raywenderlich.emitron.utils.extensions.setDataBindingView
+import com.raywenderlich.emitron.utils.getDefaultAppBarConfiguration
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -17,11 +22,27 @@ class CollectionFragment : DaggerFragment() {
 
   private val viewModel: CollectionViewModel by viewModels { viewModelFactory }
 
+  private lateinit var binding: FragmentCollectionBinding
+
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.fragment_collection, container, false)
+    binding = setDataBindingView(
+      inflater,
+      R.layout.fragment_collection,
+      container
+    )
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    initView()
+  }
+
+  private fun initView() {
+    binding.toolbar.setupWithNavController(findNavController(), getDefaultAppBarConfiguration())
   }
 }
