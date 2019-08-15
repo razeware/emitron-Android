c048849436f9460088094ac2a33c9336296caf0b
Rachit Mishra <twntee@gmail.com>
2019-08-15 20:20:17 +0530

    Add basic logging helper

---
+++ app/src/main/java/com/raywenderlich/emitron/utils/Log.kt
@@ -0,0 +1,34 @@
+package com.raywenderlich.emitron.utils
+
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+import com.raywenderlich.emitron.BuildConfig
+
+object Log {
+
+  fun debug(message: String, tag: String? = BuildConfig.APPLICATION_ID.toUpperCase()) {
+    if (BuildConfig.DEBUG || Log.isLoggable(tag, Log.DEBUG)) {
+      Log.d(tag, message)
+    }
+  }
+
+  fun exception(e: Throwable) {
+    if (BuildConfig.DEBUG) {
+      e.printStackTrace()
+    } else {
+      Crashlytics.logException(e)
+    }
+  }
+
+  fun error(e: Error) {
+    if (BuildConfig.DEBUG) {
+      e.printStackTrace()
+    } else {
+      Crashlytics.logException(e)
+    }
+  }
+
+  fun initUser(userIdentifier: String) {
+    Crashlytics.setUserIdentifier(userIdentifier)
+  }
+}
