5bce242a2f94271a4628c8ae44726585faf70983
Rachit Mishra <twntee@gmail.com>
2019-08-24 13:29:40 +0530

    Add bookmark data layer

--- app/src/main/java/com/raywenderlich/emitron/di/modules/NetModule.kt
+++ app/src/main/java/com/raywenderlich/emitron/di/modules/NetModule.kt
@@ -1,6 +1,7 @@
 package com.raywenderlich.emitron.di.modules
 
 import com.raywenderlich.emitron.BuildConfig
+import com.raywenderlich.emitron.data.bookmarks.BookmarkApi
 import com.raywenderlich.emitron.data.content.ContentApi
 import com.raywenderlich.emitron.data.login.LoginApi
 import com.raywenderlich.emitron.network.AuthInterceptorImpl
@@ -60,11 +61,16 @@ class NetModule {
 
     @JvmStatic
     @Provides
-    fun provideLoginApi(retrofit: Retrofit) = LoginApi.create(retrofit)
+    fun provideLoginApi(retrofit: Retrofit): LoginApi = LoginApi.create(retrofit)
 
     @JvmStatic
     @Provides
-    fun provideContentApi(retrofit: Retrofit) = ContentApi.create(retrofit)
+    fun provideContentApi(retrofit: Retrofit): ContentApi = ContentApi.create(retrofit)
+
+    @JvmStatic
+    @Provides
+    fun provideBookmarkApi(retrofit: Retrofit): BookmarkApi = BookmarkApi.create(retrofit)
+
 
   }
 
