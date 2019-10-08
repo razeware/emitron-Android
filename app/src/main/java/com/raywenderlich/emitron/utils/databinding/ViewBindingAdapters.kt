6192ed0ecca6f24242a83371c9e1269a60b0da05
Rachit Mishra <twntee@gmail.com>
2019-10-08 18:21:29 +0530

    Fix ui overlap with NavigationBar

--- app/src/main/java/com/raywenderlich/emitron/utils/databinding/ViewBindingAdapters.kt
+++ app/src/main/java/com/raywenderlich/emitron/utils/databinding/ViewBindingAdapters.kt
@@ -1,7 +1,10 @@
 package com.raywenderlich.emitron.utils.databinding
 
 import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.WindowInsets
 import android.widget.ImageView
+import androidx.core.view.updatePadding
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
@@ -36,6 +39,56 @@ object ViewBindingAdapters {
         .transition(
           DrawableTransitionOptions().crossFade()
         ).into(imageView)
+    }
+  }
+
+  private fun View.doOnApplyWindowInsets(f: (View, WindowInsets, InitialPadding) -> Unit) {
+    // Create a snapshot of the view's padding state
+    val initialPadding = recordInitialPaddingForView(this)
+    // Set an actual OnApplyWindowInsetsListener which proxies to the given
+    // lambda, also passing in the original padding state
+    setOnApplyWindowInsetsListener { v, insets ->
+      f(v, insets, initialPadding)
+      // Always return the insets, so that children can also use them
+      insets
+    }
+    // request some insets
+    requestApplyInsetsWhenAttached()
+  }
+
+  data class InitialPadding(
+    val left: Int, val top: Int,
+    val right: Int, val bottom: Int
+  )
+
+  private fun recordInitialPaddingForView(view: View) = InitialPadding(
+    view.paddingLeft, view.paddingTop, view.paddingRight, view.paddingBottom
+  )
+
+  @JvmStatic
+  @BindingAdapter("paddingBottomSystemWindowInsets")
+  fun applySystemWindowBottomInset(view: View, applyBottomInset: Boolean) {
+    view.doOnApplyWindowInsets { view, insets, padding ->
+      val bottom = if (applyBottomInset) insets.systemWindowInsetBottom else 0
+      view.updatePadding(bottom = padding.bottom + insets.systemWindowInsetBottom)
+    }
+  }
+
+  private fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+      // We're already attached, just request as normal
+      requestApplyInsets()
+    } else {
+      // We're not attached to the hierarchy, add a listener to
+      // request when we are
+      addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+        override fun onViewAttachedToWindow(v: View) {
+          v.removeOnAttachStateChangeListener(this)
+          v.requestApplyInsets()
+        }
+
+        override fun onViewDetachedFromWindow(v: View) = Unit
+      })
     }
   }
 }
